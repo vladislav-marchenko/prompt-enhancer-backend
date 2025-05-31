e3b582609c6fc1bda58aab2672b03cfd838e440c
Владислав <maksimsraud@gmail.com>
2025-05-31 13:11:45 +0300

    fix: added only one origin

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["*"],
+                   allow_origins=["https://chatgpt.com"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
